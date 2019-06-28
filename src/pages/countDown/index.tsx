44fd1031c0a6f386c7c31633aa712ab2a9d669d9
zhanglei <czxy408@163.com>
2019-06-28 13:24:07 +0800

    count down

--- src/pages/countDown/index.tsx
+++ src/pages/countDown/index.tsx
@@ -1,12 +1,38 @@
 import * as React from 'react';
 import styles from './index.module.less';
-
-function CountDown (): JSX.Element{
-  return (
-    <div className={styles.App}>
-     {1}
-    </div>
-  );
+interface CountDownState {
+  count: number
 }
+class CountDown extends React.Component<CountDownState> {
+
+  state: CountDownState = {
+    count: 3
+  }
+
+  componentWillMount () {
+    this.setCount()
+  }
+
+  setCount = (): void => {
+    let { count } = this.state
+    setTimeout(() => {
+      this.setState({
+        count: count - 1
+      }, () => {
+        if (this.state.count === 1) return false
+        this.setCount()
+      })
+    },1000)
+  }
+
+  render () {
+    const { count } = this.state
+    return (
+      <div className={styles.App}>{count}</div>
+    )
+  }
+}
+
+
 
 export default CountDown;
